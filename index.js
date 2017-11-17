955d47dd3d1b373dc71e26753e14cb966600b32e
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-17 01:19:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Code your solution in this file
-function findMatching(drivers, name)
+function findMatching(collection)
 {
-  return drivers.filter(function(name){ return drivers.name.toLowerCase() === name.toLowerCase();})
+  
 }
