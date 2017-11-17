f942bb25c78a5781efae9a78062d093b15367164
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-17 01:55:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file
+function findMatching(collection, name)
+{
+  const newCollection = [];
+
+
+  for (const user of collection)
+  {
+    if (user.toLowerCase() == name.toLowerCase())
+      newCollection.push(user);
+
+  }
+
+  return newCollection;
+}
+
+function fuzzyMatch(collection, likeName)
+{
+    const newCollection = [];
+    for (const user of collection)
+    {
+      if (user.lastIndexOf(likeName) === 0)
+        newCollection.push(user);
+    }
+    return newCollection;
+}
+
+function matchName(collection, name)
+{
+  const newCollection = [];
+  for (const user of collection)
+  {
+  if (user.name === name)
+    newCollection.push(user)
+  }
+  return newCollection;
+}
