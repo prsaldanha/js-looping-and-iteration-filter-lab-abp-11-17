1cc2129e726a9a7deb10385516f424f2c1a02e64
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-17 01:24:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ function findMatching(collecton, name)
 {
   const newCollection = [];
 
-/*
+
   for (const user of collection)
   {
     if (user.toLowerCase() == name.toLowerCase())
       newCollection.push(user);
-    }
+    
   }
-*/
+
 return newCollection;
 }
